a152eff126c6d69de3853ab351e6718a25203748
zokishmael <59413896+zokishmael@users.noreply.github.com>
2024-07-15 23:35:40 +0900

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -3,33 +3,20 @@ import type { Config } from './types';
 export default {
   version: '1.0',
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'Lawu 22 News',
+  shortName: 'Lawu 22',
+  description: ' Situs Informasi Kabar Papua Selatan',
   direction: 'auto',
-  language: 'en-US',
-  backgroundColor: '#fff',
-  themeColor: '#fff',
+  language: 'id-ID',
+  backgroundColor: '#ff3200',
+  themeColor: '#ff3200',
   display: 'standalone',
   orientation: 'any',
   scope: '/',
   startUrl: '/?utm_source=homescreen',
   appleStatusBarStyle: 'black-translucent',
   preferRelatedApplications: false,
-  shortcuts: [
-    {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
-    },
-    {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
-    },
-  ],
+  shortcuts: [],
   pwa: {
     logs: true,
     // OneSignal is not available if you are not using cloudflare workers
@@ -40,5 +27,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://www.lawu22.com',
 } satisfies Config;
